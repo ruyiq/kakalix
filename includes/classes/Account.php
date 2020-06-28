03bd59b1471abf8ff03148922c9bebf870f29ea5
ruyiq <ruyicassiel@gamil.com>
2020-06-27 21:49:11 -0400

    adding style to the error message

--- includes/classes/Account.php
+++ includes/classes/Account.php
@@ -24,7 +24,7 @@ class Account {
 
     public function getError($error) {
         if(in_array($error, $this->errorArray)) {
-            return $error;
+            return "<span class='errorMessage'> $error </span>";
         }
     }
 
