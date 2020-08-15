6385acfb445c2742fecfcf3429ea528255db15a6
ruyiq <ruyicassiel@gmail.com>
2020-08-15 12:46:57 -0400

    show search results

--- ajax/getSearchResult.php
+++ ajax/getSearchResult.php
@@ -1,12 +1,14 @@
 <?php
 require_once("../includes/config.php");
-require_once("../classes/searchResultProvider.php");
-require_once("../classes/entityProvider.php");
-require_once("../classes/entity.php");
+require_once("../includes/classes/entityProvider.php");
+require_once("../includes/classes/entity.php");
+require_once("../includes/classes/previewProvider.php");
+require_once("../includes/classes/searchResultProvider.php");
+require_once("../includes/classes/VideoProvider.php");
 
 if(isset($_POST["term"]) && isset($_POST["username"])) {
         $srp = new SearchResultProvider($con, $_POST["username"]);
-        echo $srp-> getResult($POST["term"]);
+        echo $srp-> getResult($_POST["term"]);
 }
 
 else {
--- includes/classes/searchResultProvider.php
+++ includes/classes/searchResultProvider.php
@@ -9,6 +9,27 @@ class SearchResultProvider{
 
     public function getResult($inputTest){
         $entities = EntityProvider::getSearchEntities($this->con, $inputTest);
+        $html = "<div class='previewCategories noScroll'>";
+        $html .= $this->getResultsHtml($entities);
+        return $html."</div>";
+    }
+
+    private function getResultsHtml($entities){
+        if(sizeof($entities) == 0) {
+            return;
+        }
+
+        $entitiesHtml = "";
+        $previewProvider = new PreviewProvider($this->con, $this->username);
+        foreach($entities as $entity) {
+            $entitiesHtml .= $previewProvider->createEntityPreviewSquare($entity);
+        }
+
+        return "<div class='category'>
+                    <div class='entities'>
+                        $entitiesHtml
+                    </div>
+                </div>";
     }
 }
 ?>
